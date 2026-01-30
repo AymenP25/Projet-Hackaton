ccc0f5ad919cda09f979ea34610978a45cd3717f
Amanda Verde Mota <amanda.verde_mota@etu.minesparis.psl.eu>
2026-01-30 13:57:25 +0100

    ff

--- map.py
+++ map.py
@@ -15,46 +15,52 @@ class GameMap:
         self.player_pos = (1, 1)
         self.ennemis = []
 
-        self._place_player()
-        self._place_enemies()
+    self._place_enemies()
 
     def _create_empty_map(self):
-        """Crée une map entourée de murs, premier jet c'est juste un cadre entouré de murs"""
+    """Crée une map entourée de murs, premier jet c'est juste un cadre entouré de murs"""
         tab = []
         for y in range(self.hauteur):
-            row = []
-            for x in range(self.largeur):
-                if x == 0 or y == 0 or x == self.largeur - 1 or y == self.hauteur - 1:
-                    row.append(WALL)
-                else:
-                    row.append(FLOOR)
-            tab.append(row)
+        row = []
+        for x in range(self.largeur):
+        if x == 0 or y == 0 or x == self.largeur - 1 or y == self.hauteur - 1:
+        row.append(WALL)
+        else:
+        row.append(FLOOR)
+        tab.append(row)
         return tab
 
-    def _place_player(self):
-        x, y = self.player_pos
-        self.tab[y][x] = PLAYER
+def _place_enemies(self):
+    placed = 0
+    while placed < self.ennemicpt:
+        x = random.randint(1, self.largeur - 2)
+        y = random.randint(1, self.hauteur - 2)
 
-    def _place_enemies(self):
-        placed = 0
-        while placed < self.ennemicpt:
-            x = random.randint(1, self.largeur - 2)
-            y = random.randint(1, self.hauteur - 2)
+    if self.tab[y][x] == FLOOR:
+        ennemi = random.choice(ENEMIES)
+        self.tab[y][x] = ennemi
+        self.ennemis.append({
+        "type": ennemi,
+        "x": x,
+        "y": y,
+        "hp": 5
+        })
+        placed += 1
 
-            if self.tab[y][x] == FLOOR:
-                ennemi = random.choice(ENEMIES)
-                self.tab[y][x] = ennemi
-                self.ennemis.append({
-                    "type": ennemi,
-                    "x": x,
-                    "y": y,
-                    "hp": 5
-                })
-                placed += 1
+    def display(game_map, hero, monster_manager):
+        for y in range(game_map.hauteur):
+            for x in range(game_map.largeur):
+                if (x, y) == (hero.x, hero.y):
+                print("@", end="")
+                elif (x, y) in monster_manager.monsters:
+                print("M", end="")
+                else:
+                print(game_map.tab[y][x], end="")
+                print()
 
-    def display(self):
-        for row in self.tab:
-            print("".join(row))
 
-game_map = GameMap(20, 10,4)
-game_map.display()
+def is_walkable(self, x, y):
+# Vérifie si la case est du sol, une porte ou un escalier
+    if 0 <= y < self.hauteur and 0 <= x < self.largeur:
+        return self.tab[y][x] in [".", "+", "=", "#"]
+    return False
